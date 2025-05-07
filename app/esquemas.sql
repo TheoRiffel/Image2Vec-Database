ab6c587ffaca05ef5587a70e34fd46c6bc033525
MurilloMMartins <murillomartins19@gmail.com>
2025-05-07 11:18:32 -0300

    Changed numeric value

--- app/esquemas.sql
+++ app/esquemas.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS metadata (
     img_rel_path character(55),
     topics character varying(50)[],
     place character varying(20),
-    income numeric(3,0),
+    income numeric(10,3),
     imagenet_synonyms character varying(50)[],
     imagenet_sysnet_id integer[]
 );
