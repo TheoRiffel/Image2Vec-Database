12ac64a46c0b639ddf808dbba6176f5889a3fb4f
TheoRiffel <theobfriffel@gmail.com>
2025-05-12 15:38:01 -0300

    indexes e constraints

--- app/esquemas.sql
+++ app/esquemas.sql
@@ -8,17 +8,31 @@ CREATE TABLE IF NOT EXISTS metadata (
     img_rel_path character(55),
     topics character varying(50)[],
     place character varying(20),
-    income numeric(10,3),
+    income numeric(10, 3),
     imagenet_synonyms character varying(50)[],
     imagenet_sysnet_id integer[]
 );
 
 CREATE TABLE IF NOT EXISTS img_pgvector (
     id character(24) PRIMARY KEY,
-    embedding vector(4096)
+    embedding vector(4096),
+    CONSTRAINT metadata_id_fkey
+        FOREIGN KEY (id)
+        REFERENCES metadata (id)
+        ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS img_pgvector (
+CREATE TABLE IF NOT EXISTS img_pgarray (
     id character(24) PRIMARY KEY,
-    embedding vector(4096)
+    embedding float8[],
+    CONSTRAINT metadata_id_fkey
+        FOREIGN KEY (id)
+        REFERENCES metadata (id)
+        ON DELETE CASCADE
 );
+
+CREATE INDEX IF NOT EXISTS country_name_idx ON metadata USING HASH (country_name);
+CREATE INDEX IF NOT EXISTS region_id_idx ON metadata USING HASH (region_id);
+CREATE INDEX IF NOT EXISTS income_idx ON metadata (income);
+
+
