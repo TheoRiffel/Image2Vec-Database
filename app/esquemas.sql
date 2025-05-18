5f612dc48e1537b56fc4971c8e9d71129a53edd1
ericarfs <ericaribeiro@usp.br>
2025-05-18 20:17:03 -0300

    Corrigindo indices nos esquemas

--- app/esquemas.sql
+++ app/esquemas.sql
@@ -53,5 +53,8 @@ CREATE INDEX IF NOT EXISTS country_name_idx ON metadata USING HASH (country_name
 CREATE INDEX IF NOT EXISTS region_id_idx ON metadata USING HASH (region_id);
 CREATE INDEX IF NOT EXISTS income_idx ON metadata (income);
 
-CREATE INDEX hnsw_idx ON img_pgvector_clip USING hnsw (embedding vector_cosine_ops);
-CREATE INDEX ivfflat_idx ON img_pgvector_clip USING ivfflat (embedding vector_cosine_ops) WITH (lists = 100);
+CREATE INDEX hnsw_idx_cosine ON img_pgvector_clip USING hnsw (embedding vector_cosine_ops);
+CREATE INDEX hnsw_idx_l1 ON img_pgvector_clip USING hnsw (embedding vector_l1_ops);
+CREATE INDEX ivfflat_idx_l2 ON img_pgvector_clip USING ivfflat (embedding vector_l2_ops) WITH (lists = 100);
+CREATE INDEX ivfflat_idx_ip ON img_pgvector_clip USING ivfflat (embedding vector_ip_ops) WITH (lists = 100);
+
